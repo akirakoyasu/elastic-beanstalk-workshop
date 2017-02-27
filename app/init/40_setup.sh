0dfa24e70e4fd31d06dbb31e976de048172091ea
Akira Koyasu <mail@akirakoyasu.net>
2017-02-27 20:13:45 +0900

    add production

--- app/init/40_setup.sh
+++ app/init/40_setup.sh
@@ -7,9 +7,14 @@ rm -f /etc/service/nginx/down
 chown -R app:app /var/www
 cd /var/www/rails-app
 setuser app bundle install --path=../vendor/bundle
+if [ "$PASSENGER_APP_ENV" = 'production' ]; then
+  setuser app bin/rails assets:precompile
+  setuser app bundle install --path=../vendor/bundle --without test development --deployment --clean
+fi
 
 SECRET_KEY_BASE=$(setuser app bin/rails secret)
 echo "passenger_env_var 'SECRET_KEY_BASE' '$SECRET_KEY_BASE';" >> /etc/nginx/conf.d/10_setenv.conf
+echo "passenger_env_var 'RDS_HOSTNAME' '$RDS_HOSTNAME';" >> /etc/nginx/conf.d/10_setenv.conf
 
 # for production, looks good .ebextensions does the things like migration.
 RAILS_ENV=$PASSENGER_APP_ENV setuser app bin/rails db:create db:migrate
