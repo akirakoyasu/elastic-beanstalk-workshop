30a62d809d055e47c680940b3a1e14b6cb4a2595
Akira Koyasu <mail@akirakoyasu.net>
2017-02-27 19:33:10 +0900

    add init script for app

---
+++ app/init/40_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+
+rm -f /etc/service/nginx/down
+
+# for production, looks good CI installs dependencies.
+chown -R app:app /var/www
+cd /var/www/rails-app
+setuser app bundle install --path=../vendor/bundle
+
+SECRET_KEY_BASE=$(setuser app bin/rails secret)
+echo "passenger_env_var 'SECRET_KEY_BASE' '$SECRET_KEY_BASE';" >> /etc/nginx/conf.d/10_setenv.conf
+
+# for production, looks good .ebextensions does the things like migration.
+RAILS_ENV=$PASSENGER_APP_ENV setuser app bin/rails db:create db:migrate
+
+sed -i -e 's!mesg n || true!mesg n 2>/dev/null || true!' /root/.profile
+
+exit 0
